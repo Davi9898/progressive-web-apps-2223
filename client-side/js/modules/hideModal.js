5dd2dc056e89379b63238d7756704218a52eae45
Davi9898 <Danielvink1998@gmail.com>
2023-04-03 23:01:24 +0200

    Verwijdering van client-side
    Herinstal node_modules

--- client-side/js/modules/hideModal.js
+++
@@ -1,7 +0,0 @@
-export function hideModal() {
-  let modal = document.querySelector('.details');
-  modal.classList.add('hidden');
-  modal.querySelector('section:nth-of-type(3) article img').src = '';
-}
-
-
